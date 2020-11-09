d2486b4ba98edad14a72b8960dd979491b54018d
Agustin Pontura <pontura@gmail.com>
2020-11-09 17:37:00 -0300

    colorizer y swaper

---
+++ src/tuenti/Assets/Colorizer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Colorizer : MonoBehaviour
+{
+    public Image[] ui_parts;
+    public SpriteRenderer[] parts;
+	public Color[] colors;
+
+	void OnEnable()
+	{
+		if (colors.Length == 0 || parts.Length == 0)
+			return;
+		Color c = colors [Random.Range (0, colors.Length)];
+
+        foreach (Image sr in ui_parts)
+        {
+            sr.color = c;
+        }
+        foreach (SpriteRenderer sr in parts) {
+			sr.color = c;
+		}
+	}
+}
