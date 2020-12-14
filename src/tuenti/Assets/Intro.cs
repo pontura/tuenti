1e47b80f52fdac957ab56c2b5952e151679b6470
Agustin Pontura <pontura@gmail.com>
2020-12-14 16:39:25 -0300

    intro  + userdata

--- src/tuenti/Assets/Intro.cs
+++ src/tuenti/Assets/Intro.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class Intro : MonoBehaviour
 {
+    Animation anim;
+    public GameObject pauseSignal;
+
+    private void Start()
+    {
+        pauseSignal.SetActive(false);
+        anim = GetComponent<Animation>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pauseSignal.SetActive(true);
+            anim[anim.clip.name].normalizedSpeed = 0;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            pauseSignal.SetActive(false);
+            anim[anim.clip.name].speed = 1;
+        }
+    }
     public void Init()
     {
         gameObject.SetActive(true);
