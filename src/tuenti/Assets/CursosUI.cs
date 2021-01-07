c1d262f2e807250497417a0537e8768c31f77098
Agustin Pontura <pontura@gmail.com>
2021-01-07 10:52:17 -0300

    Update CursosUI.cs

--- src/tuenti/Assets/CursosUI.cs
+++ src/tuenti/Assets/CursosUI.cs
@@ -56,7 +56,6 @@ public class CursosUI : UIScrollItemsScreen
                 forceUnBlock = true;
            
             id++;
-
           
 
             newButton.OnInit(cursoData, forceUnBlock);
